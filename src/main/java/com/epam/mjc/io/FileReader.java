f09e03fc6dd22ae9754b8a597de8c194c4f80db8
zhargal <bzhargal@gmail.com>
2023-01-07 15:58:35 +0300

    done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,28 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 
 public class FileReader {
+    public static Profile getDataFromFile(File file) {
+        StringBuilder stringBuilder = new StringBuilder();
+        Profile profile = new Profile();
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            int c;
+            while ((c = fileInputStream.read()) != -1) {
+                stringBuilder.append(Character.toString(c));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] values = stringBuilder.toString().split("\n");
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+        profile.setName(values[0].split(":")[1].trim());
+        profile.setAge(Integer.valueOf(values[1].split(":")[1].trim()));
+        profile.setEmail(values[2].split(":")[1].trim());
+        profile.setPhone(Long.valueOf(values[3].split(":")[1].trim()));
+        return profile;
     }
 }
